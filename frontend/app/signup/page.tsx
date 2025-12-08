b7ae56ab9c252831d592831e7bd7b3608eb2f66b
Kiran <kiran.sadara6730@gmail.com>
2025-12-08 21:02:05 +0530

    Signin and signup pages done

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -5,11 +5,12 @@ import Link from "next/link"
 import { useRouter } from "next/navigation"
 
 
-export default function Signin(){
+export default function Signup(){
 
     const[email , setEmail] = useState('')
     const[password , setPassword]= useState('')
     const[message  , SetMessage] = useState('')
+    const[username , setUsername]=useState('')
 
     function SubmitForm(e:any){
         e.preventDefault();
@@ -18,10 +19,10 @@ export default function Signin(){
        
     return(
     <div className="flex min-h-screen font-roboto items-center justify-center bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 px-4">
-  <div className="w-full max-w-md sm:max-w-lg bg-gradient-to-tr from-white/10 to-white/5 hover:to-white/15 backdrop-blur-xl border border-white/20 rounded-3xl p-10 shadow-2xl">
+  <div className="w-full max-w-md sm:max-w-lg bg-gradient-to-tr from-white/10 to-white/5 backdrop-blur-xl border border-white/20 rounded-3xl p-10 shadow-2xl hover:from-white/15">
     
     <h1 className="text-center text-xl sm:text-2xl  font-extrabold text-blue-500 mb-10 ">
-       Welcome Back
+       Register Here
     </h1>
 
     <form className="space-y-6" onSubmit={SubmitForm}>
@@ -33,7 +34,18 @@ export default function Signin(){
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Enter your email"
-          className="w-full px-5 py-3 rounded-2xl bg-white/20 border border-white/30 text-white placeholder-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 transition-all backdrop-blur-sm shadow-inner"
+          className="w-full px-4 py-3 rounded-2xl bg-white/20 border border-white/30 text-white placeholder-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 transition-all backdrop-blur-sm shadow-inner"
+        />
+      </div>
+       <div className="flex flex-col">
+        <label className="text-lg sm:text-xl font-semibold mb-2 text-gray-200">Username</label>
+        <input
+          type="text"
+          required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Enter your username"
+          className="w-full px-4 py-3 rounded-2xl bg-white/20 border border-white/30 text-white placeholder-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 transition-all backdrop-blur-sm shadow-inner"
         />
       </div>
 
@@ -45,27 +57,23 @@ export default function Signin(){
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Enter your password"
-          className="w-full px-5 py-3 rounded-2xl bg-white/20 border border-white/30 text-white placeholder-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 transition-all backdrop-blur-sm shadow-inner"
+          className="w-full px-4 py-3 rounded-2xl bg-white/20 border border-white/30 text-white placeholder-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 transition-all backdrop-blur-sm shadow-inner"
         />
       </div>
 
       <button className="w-full py-3 rounded-2xl bg-gradient-to-r from-blue-500 to-blue-600 text-white font-bold text-lg sm:text-xl shadow-lg hover:shadow-xl hover:scale-105 transform transition-all duration-300">
-        Login
+        Register
       </button>
     </form>
 
     <div className="mt-6 flex flex-col items-center space-y-3 text-center">
       <p className="text-gray-300 text-sm sm:text-base">
-        Don’t have an account? {' '}
-        <Link href="/signup" className="text-blue-400 hover:text-blue-300 hover:underline transition">
+        Login Here ? {' '}
+        <Link href="/signin" className="text-blue-400 hover:text-blue-300 hover:underline transition">
           Sign up
         </Link>
       </p>
-      <p className="text-gray-300 text-sm sm:text-base px-4 py-2 rounded-lg hover:bg-white/10 transition">
-        <Link href="/forgetpassword" className="text-blue-400 hover:text-blue-300 hover:underline transition">
-          Forgot password?
-        </Link>
-      </p>
+      
     </div>
 
     {message && (
